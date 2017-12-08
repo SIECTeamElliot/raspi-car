527779277181ba5058692d25a906ff4da16a7ce9
Johan Medrano <jmsnowrider@gmail.com>
2017-12-08 15:54:02 +0100

    rm build from versionning

--- build/CMakeFiles/exec.dir/cmake_clean.cmake
+++ build/CMakeFiles/exec.dir/cmake_clean.cmake
@@ -2,8 +2,8 @@ file(REMOVE_RECURSE
   "CMakeFiles/exec.dir/src/init_all.c.o"
   "CMakeFiles/exec.dir/src/communicationSTM.c.o"
   "CMakeFiles/exec.dir/src/listen.c.o"
-  "build/bin/exec.pdb"
-  "build/bin/exec"
+  "bin/exec.pdb"
+  "bin/exec"
 )
 
 # Per-language clean rules from dependency scanning.
